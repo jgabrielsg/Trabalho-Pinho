fff50e1b7e97a3fbe6b1a15414c1f34ca8579074
Vinasque <b51986@fgv.edu.br>
2023-06-18 22:28:25 -0300

    Mudei a base e criei duas funções

--- datacleaning.py
+++ datacleaning.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv("gas_prices.csv")
+# Função que, quando dado o nome do arquivo csv, te retorna ele como um dataset em Pandas:
+def criar_dataset(dataset):
+    dataset_preparado = pd.read_csv(dataset)
+    return dataset_preparado
 
-# Testando se os valores estão completos:
+# Confere se há valores vazios em alguma coluna:
+def coluna_vazia(dataset):
+    for coluna in dataset:
+        valor_vazio = dataset[coluna].isna().any() # Retorna um booleano que mostra ter ou não valores vazios em alguma célula
+        print("A coluna '{}' tem algum valor vazio? {}".format(coluna, valor_vazio))
 
-for coluna in df:
-    coluna_vazia = df[coluna].isna().any() # Retorna um booleano que mostra ter ou não valores vazios em alguma célula de cada coluna
-    print("A coluna '{}' tem algum valor vazio? {}".format(coluna, coluna_vazia))
+df = criar_dataset("prouni.csv")
+coluna_vazia(df)
 
-print("\n"*3)
-# Dataset completo!
+print("-=-="*19)
