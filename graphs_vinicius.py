53255922194c8ee39e917daadd96ad710c99753a
Vinasque <b51986@fgv.edu.br>
2023-06-21 22:54:22 -0300

    Melhorei mais ainda o meu primeiro gráfico

--- graphs_vinicius.py
+++ graphs_vinicius.py
@@ -2,11 +2,13 @@ from datacleaning import criar_dataset, coluna_vazia, limpar_coluna, contar_repe
 from bokeh.models import ColumnDataSource, NumeralTickFormatter, Range1d
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
+from bokeh.palettes import Category10_10
 
 df = criar_dataset("prouni.csv")
 
 output_file("teste_vinicius.html")
 
+"""
 # Primeiro gráfico: quantidade de bolsistas por ano
 
 contagem_por_ano = contar_repeticoes(df, 'ANO_CONCESSAO_BOLSA') # Dataframe só com o ano e a quantidade de vezes que ele aparece
@@ -28,4 +30,30 @@ plot.yaxis.axis_label = 'Quantidade'
 plot.yaxis.formatter = NumeralTickFormatter(format='0,0') # Impede que os números apareçam em notação científica
 
 # Exibir o gráfico
+show(plot)
+"""
+
+tipo_por_ano = df.groupby(['TIPO_BOLSA', 'ANO_CONCESSAO_BOLSA']).size().reset_index(name='QUANTIDADE')
+
+# Apenas em 1 ano teve bolsas de 25% e esse valor único não está sendo plotado no gráfico, então vou tirar para não atrapalhar
+tipo_por_ano = tipo_por_ano.drop(tipo_por_ano[tipo_por_ano['TIPO_BOLSA'] == 'BOLSA COMPLEMENTAR 25%'].index)
+
+plot = figure()
+
+# Criando uma fonte de dados para cada tipo de bolsa
+bolsas = {}
+for tipo_bolsa in tipo_por_ano['TIPO_BOLSA'].unique():
+    tipo_bolsa_data = tipo_por_ano[tipo_por_ano['TIPO_BOLSA'] == tipo_bolsa]
+    bolsas[tipo_bolsa] = ColumnDataSource(tipo_bolsa_data)
+
+# Plotando as linhas
+for tipo_bolsa, color in zip(tipo_por_ano['TIPO_BOLSA'].unique(), Category10_10):
+    plot.line(x='ANO_CONCESSAO_BOLSA', y='QUANTIDADE', source=bolsas[tipo_bolsa], line_color=color, legend_label=tipo_bolsa, width=2)
+
+# Adicionando a legenda
+plot.legend.location = 'top_left'
+plot.legend.title = 'TIPO DE BOLSA'
+plot.yaxis.formatter = NumeralTickFormatter(format='0,0') # Impede que os números apareçam em notação científica
+
+# Exibindo o gráfico
 show(plot)
