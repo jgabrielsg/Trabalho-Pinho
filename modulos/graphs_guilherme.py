49853a111987c679f2b4b09687d49f706c55d11e
Guilherme Buss <guibuss10@gmail.com>
2023-06-27 20:57:26 -0300

    Minha primeira ideia para segundo gráfico.

--- modulos/graphs_guilherme.py
+++ modulos/graphs_guilherme.py
@@ -14,6 +14,7 @@ DATA = 'CSVs/prouni.csv'
 df = criar_dataset(DATA)
 
 #------Primeiro Gráfico: Relação entre região e acesso de Deficientes físicos a bolsas
+
 def guilherme_plot1(df):
     #Filtra os dados para as linhas da coluna 'BENEFICIARIO_DEFICIENTE_FISICO' com valor igual a "sim".
     df_filtrado = df[df['BENEFICIARIO_DEFICIENTE_FISICO'] == 'sim']
@@ -28,7 +29,7 @@ def guilherme_plot1(df):
     plot1 = figure(x_range=df1["REGIAO_BENEFICIARIO_BOLSA"], width=1000, height=480, tools="box_zoom, pan, reset")
     plot1.xaxis.axis_label = "Região"
     plot1.yaxis.axis_label = "Número de benificiados"
-    plot1.title.text = "Número de deficientes com acesso a bolsa por região"
+    plot1.title.text = "Número de deficientes físicos com acesso a bolsa por região"
 
     #Gráfico de barras da região pela quantidade de bolsistas com deficiência física.
     plot1.vbar(x="REGIAO_BENEFICIARIO_BOLSA", top="Quantidade", width=0.4, line_width=2,
@@ -38,3 +39,40 @@ def guilherme_plot1(df):
     plot1.y_range.start = 0
 
     return plot1
+
+#------Segundo Gráfico: Relação entre Estado e Munícipio com o acesso de Deficientes físicos a bolsas no Sudeste brasileiro
+
+def guilherme_plot2(df):
+    #Filtrando os dados para a região sudeste.
+    df_sudeste = df[df['REGIAO_BENEFICIARIO_BOLSA'] == 'SUDESTE']
+    #Separando em grupos por estado e medidindo a quantidade de bolsistas.
+    df_sudeste = df_sudeste.groupby("SIGLA_UF_BENEFICIARIO_BOLSA").size().reset_index(name='Quantidade_total')
+
+    #Filtrando os dados para bolsistas com deficiência física do sudeste.
+    df_deficientes_fisicos_sudeste =df.loc[(df['REGIAO_BENEFICIARIO_BOLSA'] == 'SUDESTE') & (df['BENEFICIARIO_DEFICIENTE_FISICO'] == 'sim')]
+    #Medidindo a quantidade de bolsistas específicos.
+    df_deficientes_fisicos_sudeste = df_deficientes_fisicos_sudeste.groupby("SIGLA_UF_BENEFICIARIO_BOLSA").size().reset_index(name='Quantidade')
+
+    #Calcula a proporção dos bolsistas com deficiência física com o total de bolsistas por estado.
+    df_deficientes_fisicos_sudeste['Proporcao'] = df_deficientes_fisicos_sudeste['Quantidade']/df_sudeste['Quantidade_total']
+
+    #Paleta de cores aleatórias
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+
+    #Estrutura básica do gráfico
+    plot2 = figure(x_range=df_deficientes_fisicos_sudeste["SIGLA_UF_BENEFICIARIO_BOLSA"], width=1000, height=480, tools="box_zoom, pan, reset")
+    plot2.xaxis.axis_label = "Estado"
+    plot2.yaxis.axis_label = "Número de benificiados(%)"
+    plot2.title.text = "Número de deficientes físicos com acesso a bolsa em relação ao total de bolsistas por estado da região sudeste"
+
+    #Gráfico de barras do estado pela proporção de bolsistas com deficiência física.
+    plot2.vbar(x="SIGLA_UF_BENEFICIARIO_BOLSA", top="Proporcao", width=0.4, line_width=2,
+            #Preenchendo as barras com uma cor para cada região.
+            fill_color=factor_cmap("SIGLA_UF_BENEFICIARIO_BOLSA", palette=colors, factors=df_deficientes_fisicos_sudeste["SIGLA_UF_BENEFICIARIO_BOLSA"].unique()),source=df_deficientes_fisicos_sudeste)
+
+    plot2.y_range.start = 0
+
+    return plot2
+
+show(guilherme_plot2(df))
+# print(df.columns)
