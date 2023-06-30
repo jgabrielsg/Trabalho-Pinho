aafb5d837ecfebd758a2f3241bde8c86cffef51f
Guilherme Buss <guibuss10@gmail.com>
2023-06-29 22:55:50 -0300

    Fiz um terceiro gráfico no formato Candlestick Shart

--- modulos/graphs_guilherme.py
+++ modulos/graphs_guilherme.py
@@ -7,10 +7,6 @@ from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
 from bokeh.layouts import gridplot
 from bokeh.palettes import Category20
-
-import sys
-sys.path.append('modulos/')
-
 from datacleaning import criar_dataset
 
 output_file("Testes/teste_guilherme.html")
@@ -21,9 +17,7 @@ df = criar_dataset(DATA)
 
 #------Primeiro Gráfico: Relação entre região e acesso de Deficientes físicos a bolsas
 
-from bokeh.plotting import figure
-
-def guilherme_plot1(df):
+def Guilherme_plot1(df):
     # Filtra os dados para bolsistas com deficiência física.
     df_filtrado = df[df['BENEFICIARIO_DEFICIENTE_FISICO'] == 'sim']
 
@@ -56,7 +50,7 @@ def guilherme_plot1(df):
 
 #------Segundo Gráfico: Relação entre Estado e Munícipio com o acesso de Deficientes físicos a bolsas no Sudeste brasileiro
 
-def guilherme_plot2(df):
+def Guilherme_plot2(df):
     #Filtrando os dados para a região sudeste.
     df_sudeste = df[df['REGIAO_BENEFICIARIO_BOLSA'] == 'SUDESTE']
     #Separando em grupos por estado e medidindo a quantidade de bolsistas.
@@ -102,6 +96,53 @@ def guilherme_plot2(df):
     plot2 = gridplot([[plot_desproporcional, None],[plot_proporcional, None]])          
 
     return plot2
- 
-show(guilherme_plot2(df))
+
+#------Terceiro Gráfico: fazer um gráfico da quantidade de registros por ano no estilo gráfico de velas
+
+def Guilherme_plot3(df):
+    # Separa os dados por ano e mede a quantidade de bolsistas.
+    dados_por_ano = df.groupby(['ANO_CONCESSAO_BOLSA']).size()
+
+    # Gera uma lista com os anos.
+    anos = dados_por_ano.index.tolist()
+    # Conta o número de anos.
+    num_anos = len(dados_por_ano)-1
+
+    # Define o eixo x, para ser YYYY-YYYY, já que para fazer as "velas" precisamos de dois valores(dois anos).
+    eixo_x = []
+    for i in range(num_anos):
+        eixo_x.append(str(anos[i]) + '-' + str(anos[i+1]))
+
+    # Cria o gráfico.
+    plot3 = figure(x_range=eixo_x, height=400, title="Variação de bolsistas por ano")
+
+    # Cria duas listas, uma com as bases: o menor dos resultados, outra com os topos: o maior deles.
+    bases_das_velas = []
+    topos_das_velas = []
+    for i in range(num_anos):
+        bases_das_velas.append(min(dados_por_ano.iloc[i+1], dados_por_ano.iloc[i]))
+        topos_das_velas.append(max(dados_por_ano.iloc[i+1], dados_por_ano.iloc[i]))
+
+    # Checa se a quantidade de bolsistas/dados está aumentando ou diminuindo, e adiciona a lista cores a cor verde ou vermelha respectivamente.
+    cores = []
+    for i in range(num_anos):
+        if dados_por_ano.iloc[i+1] > dados_por_ano.iloc[i]:
+            cores.append('green')
+        else:
+            cores.append('red')
+
+    # Gera as linhas que cortam as velas.
+    for i in range(num_anos):
+        if num_anos > i > 0:
+            plot3.segment(x0=eixo_x[i], x1=eixo_x[i], y0=bases_das_velas[i-1]-bases_das_velas[i+1], y1=topos_das_velas[i-1]-topos_das_velas[i+1], color='black', line_width=2)
+        else:
+            break
+    # plot3.segment(x0=eixo_x, x1=eixo_x, y0=bases_das_velas, y1=topos_das_velas, color='black', line_width=2)
+
+    # Gera as velas.
+    plot3.vbar(x=eixo_x, width=0.7, bottom=bases_das_velas, top=topos_das_velas, fill_color=cores, line_color='black')
+
+    return plot3
+
+show(Guilherme_plot3(df))
 # print(df.columns)
